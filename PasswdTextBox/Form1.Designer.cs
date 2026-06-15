8b47386a42517f02135d4fd7fe7f3f6d4e26b63c
Rikard Carlsson <rikard.carlsson@iths.se>
2026-06-15 20:23:45 +0200

    asdfsadf

--- PasswdTextBox/Form1.Designer.cs
+++ PasswdTextBox/Form1.Designer.cs
@@ -20,20 +20,68 @@
             base.Dispose(disposing);
         }
 
-        #region Windows Form Designer generated code
+		#region Windows Form Designer generated code
 
-        /// <summary>
-        ///  Required method for Designer support - do not modify
-        ///  the contents of this method with the code editor.
-        /// </summary>
-        private void InitializeComponent()
-        {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
-        }
+		/// <summary>
+		///  Required method for Designer support - do not modify
+		///  the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			tbPasswd = new TextBox();
+			lblShowPasswd = new Label();
+			btnShowMe = new Button();
+			SuspendLayout();
+			// 
+			// tbPasswd
+			// 
+			tbPasswd.Location = new Point(20, 25);
+			tbPasswd.Name = "tbPasswd";
+			tbPasswd.PasswordChar = '*';
+			tbPasswd.Size = new Size(245, 31);
+			tbPasswd.TabIndex = 0;
+			tbPasswd.TextChanged += textBox1_TextChanged;
+			// 
+			// lblShowPasswd
+			// 
+			lblShowPasswd.BorderStyle = BorderStyle.Fixed3D;
+			lblShowPasswd.Location = new Point(20, 75);
+			lblShowPasswd.Name = "lblShowPasswd";
+			lblShowPasswd.Size = new Size(245, 31);
+			lblShowPasswd.TabIndex = 1;
+			lblShowPasswd.Text = "MyPassword";
+			lblShowPasswd.Click += label1_Click;
+			// 
+			// btnShowMe
+			// 
+			btnShowMe.Location = new Point(83, 152);
+			btnShowMe.Name = "btnShowMe";
+			btnShowMe.Size = new Size(100, 40);
+			btnShowMe.TabIndex = 2;
+			btnShowMe.Text = "Show Me";
+			btnShowMe.UseVisualStyleBackColor = true;
+			btnShowMe.Click += btnShowMe_Click;
+			// 
+			// Form1
+			// 
+			AutoScaleDimensions = new SizeF(10F, 25F);
+			AutoScaleMode = AutoScaleMode.Font;
+			ClientSize = new Size(298, 270);
+			Controls.Add(btnShowMe);
+			Controls.Add(lblShowPasswd);
+			Controls.Add(tbPasswd);
+			Location = new Point(90, 150);
+			Name = "Form1";
+			Text = "PasswdTextBox";
+			Load += Form1_Load;
+			ResumeLayout(false);
+			PerformLayout();
+		}
 
-        #endregion
-    }
+		#endregion
+
+		private TextBox tbPasswd;
+		private Label lblShowPasswd;
+		private Button btnShowMe;
+	}
 }
--- PasswdTextBox/Form1.cs
+++ PasswdTextBox/Form1.cs
@@ -1,10 +1,30 @@
 namespace PasswdTextBox
 {
-    public partial class Form1 : Form
-    {
-        public Form1()
-        {
-            InitializeComponent();
-        }
-    }
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		private void Form1_Load(object sender, EventArgs e)
+		{
+
+		}
+
+		private void textBox1_TextChanged(object sender, EventArgs e)
+		{
+
+		}
+
+		private void label1_Click(object sender, EventArgs e)
+		{
+
+		}
+
+		private void btnShowMe_Click(object sender, EventArgs e)
+		{
+			lblShowPasswd.Text = tbPasswd.Text;
+		}
+	}
 }
