e3e46d34c6ae4ce85d9a991a5a14777694e2c795
Rikard Carlsson <rikard.carlsson@iths.se>
2026-07-03 21:52:58 +0200

    memory

--- Memory/Form1.Designer.cs
+++ Memory/Form1.Designer.cs
@@ -20,26 +20,40 @@
             base.Dispose(disposing);
         }
 
-        #region Windows Form Designer generated code
+		#region Windows Form Designer generated code
 
-        /// <summary>
-        ///  Required method for Designer support - do not modify
-        ///  the contents of this method with the code editor.
-        /// </summary>
-        private void InitializeComponent()
-        {
-            SuspendLayout();
-            // 
-            // Form1
-            // 
-            AutoScaleDimensions = new SizeF(7F, 15F);
-            AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(744, 728);
-            Name = "Form1";
-            Text = "Form1";
-            ResumeLayout(false);
-        }
+		/// <summary>
+		///  Required method for Designer support - do not modify
+		///  the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			label1 = new Label();
+			SuspendLayout();
+			// 
+			// label1
+			// 
+			label1.AutoSize = true;
+			label1.Location = new Point(20, 9);
+			label1.Name = "label1";
+			label1.Size = new Size(71, 15);
+			label1.TabIndex = 0;
+			label1.Text = "Antal försök";
+			// 
+			// Form1
+			// 
+			AutoScaleDimensions = new SizeF(7F, 15F);
+			AutoScaleMode = AutoScaleMode.Font;
+			ClientSize = new Size(744, 728);
+			Controls.Add(label1);
+			Name = "Form1";
+			Text = "Memory";
+			ResumeLayout(false);
+			PerformLayout();
+		}
 
-        #endregion
-    }
+		#endregion
+
+		private Label label1;
+	}
 }
