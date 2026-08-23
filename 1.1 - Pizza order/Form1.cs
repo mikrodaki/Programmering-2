b904546fe3dd0dbc2687195c68e4e30301c54a1f
Rikard Carlsson <rikard.carlsson@iths.se>
2026-08-23 17:03:13 +0200

    volym

--- 1.1 - Pizza order/Form1.cs
+++ 1.1 - Pizza order/Form1.cs
@@ -1,5 +1,3 @@
-using PdfSharp.Drawing; 
-
 namespace _1._1___Pizza_order
 {
     public partial class Form1 : Form
---
+++ 1.2 - ArgumentOutOfRange 2/Program.cs
@@ -0,0 +1,40 @@
+﻿using App.IO;
+
+namespace _1._2___ArgumentOutOfRange_2
+{
+	internal class Program
+	{
+		static int volume = 0;
+		static void Main(string[] args)
+		{
+			while (true)
+			{
+				ConsoleHelper.ClearScreen("Set volume");
+				Console.WriteLine($"Volymen just nu är {volume}");
+				int newVolume = ConsoleHelper.ReadInt("Ange den nya volymen: ", false);
+				try
+				{
+					SetVolume(newVolume);
+					Console.WriteLine($"Den nya volymen är: {volume}");
+				}
+				catch (ArgumentOutOfRangeException e)
+				{
+					Console.WriteLine(e.Message);
+					Console.ReadKey();
+				}
+
+				Console.WriteLine("Tryck ESC för att avsluta eller valfri tangent för att fortsätta.");
+
+				if (Console.ReadKey(true).Key == ConsoleKey.Escape)
+					break;
+			}
+		}
+
+		static void SetVolume(int value)
+		{
+			if (value < 0 || value > 100)
+				throw new ArgumentOutOfRangeException(null, "Volymen måste vara mellan 0 och 100");
+			volume = value;
+		}
+	}
+}
