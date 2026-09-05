97cc308536032213a4095c0e0b3cf4097ea3c4e1
Rikard Carlsson <rikard.carlsson@iths.se>
2026-09-05 21:36:13 +0200

    asdf

--- SLASK/Program.cs
+++ SLASK/Program.cs
@@ -4,63 +4,69 @@
 	{
 		static void Main(string[] args)
 		{
-			//double? result = FactorialDouble(50243782347);
-			//Console.WriteLine(result?.ToString() ?? "null");
-			Console.WriteLine(Pow(5, -3));
+			ShowFolders(@"C:\Test");
+		}
 
-			static int Factorial(int n)
+		static int Factorial(int n)
+		{
+			if (n == 1)
+				return 1;
+
+			return n * Factorial(n - 1);
+		}
+
+		static void CountDown(int n)
+		{
+			if (n < 0)
+				return;
+
+			Console.WriteLine(n);
+
+			CountDown(n - 1);
+		}
+
+		static int Fibonacci(int n)
+		{
+			Console.WriteLine($"F({n})");
+
+			if (n <= 1)
+				return 1;
+
+			return Fibonacci(n - 1) + Fibonacci(n - 2);
+		}
+
+		static double? FactorialDouble(double n)
+		{
+			if (n < 0 || n > 170)
+				return null;
+
+			if (n % 1 != 0)
+				return null;
+
+			if (n <= 1)
+				return 1;
+
+			return n * FactorialDouble(n - 1);
+		}
+
+		static int Pow(int a, int n)
+		{
+			if (n < 1)
+				return 1;
+
+			return a * Pow(a, n - 1);
+		}
+
+		static void ShowFolders(string path)
+		{
+			Console.WriteLine(path);
+
+			foreach (string folder in Directory.GetDirectories(path))
 			{
-				if (n == 1)
-					return 1;
-
-				return n * Factorial(n - 1);
-			}
-
-			static void CountDown(int n)
-			{
-				if (n < 0)
-					return;
-
-				Console.WriteLine(n);
-
-				CountDown(n - 1);
-			}
-
-			static int Fibonacci(int n)
-			{
-				Console.WriteLine($"F({n})");
-
-				if (n <= 1)
-					return 1;
-
-				return Fibonacci(n - 1) + Fibonacci(n - 2);
-			}
-
-
-			static double? FactorialDouble(double n)
-			{
-				if (n < 0 || n > 170)
-					return null;
-
-				if (n % 1 != 0)
-					return null;
-
-				if (n <= 1)
-					return 1;
-
-				return n * FactorialDouble(n - 1);
-			}
-
-
-			static int Pow(int a, int n)
-			{
-				if (n < 1)
-					return 1;
-
-				return a * Pow(a, n - 1);
+				ShowFolders(folder);
 			}
 		}
 	}
-
-    // Ändrade lite i slask för att se om GIT fungerar som det ska.
 }
+
+// Ändrade lite i slask för att se om GIT fungerar som det ska.
